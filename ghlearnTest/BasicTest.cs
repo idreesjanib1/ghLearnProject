340047ea766f0a56b8ede7573cbf57208659da68
ijanib <ijanib@LUL027989723257>
2025-08-13 15:02:22 +0200

    Add project files.

---
+++ ghlearnTest/BasicTest.cs
@@ -0,0 +1,17 @@
+﻿namespace ghlearnTest
+{
+    public class BasicTest
+    {
+        [Fact(DisplayName = "test method 1")]
+        public void Test1()
+        {
+            string[] Summaries = new[]
+            {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+            Assert.NotEmpty(Summaries);
+            Assert.Single(Summaries);
+        }
+    }
+}
